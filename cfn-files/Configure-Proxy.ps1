c86da889c4059a8e0b19b61f6f75853cfe358ccb
Nameless <nameless@namelesss-mbp.attlocal.net>
2019-10-26 20:47:29 -0500

    Separated ps1 files and reorganized script execution order

---
+++ cfn-files/Configure-Proxy.ps1
@@ -0,0 +1,31 @@
+# Author: Roberto Rodriguez (@Cyb3rWard0g)
+# License: GPL-3.0
+# Updated: Josh Hawkins (@BinaryFaultline)
+
+# References:
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [String]$LocalAdminPassword,
+    [Parameter(Mandatory=$true)]
+    [String]$ProxyIP
+
+)
+
+# Setting the proxy server
+$ProxyHost = "http://" + $ProxyIP + ":3128"
+Write-Host "Setting the proxy server to $ProxyHost"
+
+
+Set-ItemProperty -Force -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyEnable" -Value 1 -Type dword
+Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyEnable"
+
+Set-ItemProperty -Force -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "AutoDetect" -Value 0 -Type dword
+Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "AutoDetect"
+
+Set-ItemProperty -Force -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyServer" -Value $ProxyHost
+Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyServer"
+
+Set-ItemProperty -Force -Path "HKLM:\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxySettingsPerUser" -Value 0 -Type dword
+Get-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxySettingsPerUser"
