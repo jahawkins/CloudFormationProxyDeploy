c85b62be3664fbabd53d7303e8e6d3faff9854bd
Nameless <nameless@namelesss-mbp.attlocal.net>
2019-10-26 20:07:50 -0500

    PS logging

--- cfn-files/Prepare-Box.ps1
+++ cfn-files/Prepare-Box.ps1
@@ -79,13 +79,18 @@ Write-Host "Setting WDigest to use logoncredential.."
 Set-ItemProperty -Force -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest" -Name "UseLogonCredential" -Value "1"
 
 # Setting the proxy server
-Write-Host "Setting the proxy server"
 $ProxyHost = "http://" + $ProxyIP + ":3128"
+Write-Host "Setting the proxy server to $ProxyHost"
+
 
 Set-ItemProperty -Force -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyEnable" -Value 1 -Type dword
+Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyEnable"
 
 Set-ItemProperty -Force -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "AutoDetect" -Value 0 -Type dword
+Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "AutoDetect"
 
 Set-ItemProperty -Force -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyServer" -Value $ProxyHost
+Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxyServer"
 
 Set-ItemProperty -Force -Path "HKLM:\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxySettingsPerUser" -Value 0 -Type dword
+Get-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "ProxySettingsPerUser"
